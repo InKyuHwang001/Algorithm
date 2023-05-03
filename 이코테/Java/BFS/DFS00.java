e86b6170d2cf82feef2894153e924d00350d2ee0
InGyu Hwang <hwanginkyu42@naver.com>
2023-05-03 22:10:41 +0900

    BFS/DFS

--- 이코테/Java/BFS/DFS00.java
+++ 이코테/Java/BFS/DFS00.java
@@ -55,3 +55,75 @@ public class DFS00 {
         System.out.println(result);
     }
 }
+//---------------------------------------
+class Node{
+    private int x;
+    private int y;
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class DFS2BFS {
+
+    public static int n, m;
+    public static int[][] graph = new int[1000][1000];
+
+    public static int dx[] = {-1, 0, 1, 0};
+    public static int dy[] = {0, -1, 0, 1};
+
+    public static int bfs(int x, int y) {
+
+        Queue<Node> q = new LinkedList<>();
+        q.offer(new Node(x, y));
+
+        while(!q.isEmpty()) {
+            Node node = q.poll();
+            x = node.getX();
+            y = node.getY();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                if (graph[nx][ny] == 0) continue;
+                if (graph[nx][ny] == 1) {
+                    graph[nx][ny] = graph[x][y] + 1;
+                    q.offer(new Node(nx, ny));
+                } 
+            }
+        }
+        return graph[n - 1][m - 1];
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // N, M을 공백을 기준으로 구분하여 입력 받기
+        n = sc.nextInt();
+        m = sc.nextInt();
+        sc.nextLine(); // 버퍼 지우기
+
+        // 2차원 리스트의 맵 정보 입력 받기
+        for (int i = 0; i < n; i++) {
+            String str = sc.nextLine();
+            for (int j = 0; j < m; j++) {
+                graph[i][j] = str.charAt(j) - '0';
+            }
+        }
+
+        System.out.println(bfs(0, 0));
+    }
+}
