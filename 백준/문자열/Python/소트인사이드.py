a82486152855f89f89133194ecaea1d1d9b46d33
InGyu Hwang <hwanginkyu42@naver.com>
2023-07-06 12:07:44 +0900

    문자열

---
+++ 백준/문자열/Python/소트인사이드.py
@@ -0,0 +1,15 @@
+#소트인사이드
+##
+arr = list(input())
+arr.sort(reverse=True)
+tmp = ''
+for a in arr:
+  tmp += a
+print(tmp)
+
+##
+li = list(map(int, input()))
+li.sort(reverse=True)
+
+for i in li:
+    print(i,end='')
