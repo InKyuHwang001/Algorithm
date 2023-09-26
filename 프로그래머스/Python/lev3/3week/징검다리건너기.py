50ddfc30c7f43b2bec9be7edd51160fc25f7b43b
InGyu Hwang <hwanginkyu42@naver.com>
2023-09-26 23:54:33 +0900

    pro Lv3

--- 프로그래머스/Python/lev3/3week/징검다리건너기.py
+++ 프로그래머스/Python/lev3/3week/징검다리건너기.py
@@ -1,21 +1,20 @@
-from collections import deque
-def solution(n, edge):
-    answer = 0
-    graph = [[]for _ in range(n+1)]
-    for a,b in edge:
-        graph[a].append(b)
-        graph[b].append(a)
-    visited = [-1] * (n+1)
-    visited[1] = 0
-    q = deque()
-    q.append(1)
-    
-    while q:
-        en = q.popleft()
-        for next in graph[en]:
-            if visited[next] == -1:
-                visited[next] = visited[en] + 1
-                q.append(next)
-    visited.sort(reverse=True)
-    answer = visited.count(visited[0])
-    return answer
+def solution(stones, k):
+    left = 1
+    right = 200000000
+    while left <= right:
+        temp = stones.copy()
+        mid = (left + right) // 2
+        cnt = 0
+        for t in temp:
+            if t - mid <= 0:
+                cnt += 1
+            else:
+                cnt = 0
+            if cnt >= k:
+                break
+        if cnt >= k:
+            right = mid - 1
+        else:
+            left = mid + 1
+        
+    return left
