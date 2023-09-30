4cd5a2513481e1bb7e121120c147036d4c209485
InGyu Hwang <hwanginkyu42@naver.com>
2023-09-30 15:03:20 +0900

    pro Lv3

--- 프로그래머스/Python/lev3/3week/징검다리건너기.py
+++ 프로그래머스/Python/lev3/3week/징검다리건너기.py
@@ -1,20 +1,23 @@
+#이분탐색
 def solution(stones, k):
-    left = 1
-    right = 200000000
-    while left <= right:
-        temp = stones.copy()
-        mid = (left + right) // 2
-        cnt = 0
-        for t in temp:
-            if t - mid <= 0:
-                cnt += 1
+    answer = 0
+    smin, smax = 0, max(stones)
+    
+    while smin<= smax:
+        ct=0
+        mid = (smin + smax) //2
+        for s in stones:
+            if (s-mid) <= 0:
+                ct+=1
             else:
-                cnt = 0
-            if cnt >= k:
+                ct = 0
+            if ct>= k :
                 break
-        if cnt >= k:
-            right = mid - 1
+
+        if ct < k:
+            smin = mid + 1
         else:
-            left = mid + 1
-        
-    return left
+            answer = mid
+            smax = mid -1
+            
+    return answer
