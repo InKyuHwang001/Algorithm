62bd76c2ad9a1fadd42dadc53e75497c08254726
InKyuHwang001 <hwanginkyu42@naver.com>
2024-05-30 12:27:54 +0900

    0530

---
+++ 프로그래머스/Java/Lev1/Sol0000.java
@@ -0,0 +1,24 @@
+public class Sol0000 {
+   public static void main(String[] args) {
+        var q1 = new Q1();
+        var q2 = new Q2();
+        var q3 = new Q3();
+
+        q1.solution("123");
+
+    }
+}
+
+class Q1 {
+    public int solution(String s) {
+
+        return Integer.parseInt(s);
+    }
+}
+
+class Q2 {
+
+}
+class Q3 {
+
+}
